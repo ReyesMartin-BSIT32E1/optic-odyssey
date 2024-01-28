3ea2eee16d104f53f32820a3a343b13a290e334c
Smarty0917 <martintristanmreyes@gmail.com>
2024-01-28 16:13:45 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,58 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div bgcolor="#e9e9e9">
+<div>
+   
+    <ul className="no-bullets"> 
+       
+        <li className="logo"><img src="log.png" alt="Logo"/></li>
+      
+        <li><a href="Gallery.html">Gallery</a></li>
+        
+        <li><a href="">Home</a></li>
+    </ul>
+    <img src="header.JPG" className="nav-image"/>
+</div>
+
+<br/>
+
+<div>
+    
+    <br/>
+    <br/>
+    <br/>
+</div>
+
+<div>
+    <table>
+        <tr>
+            <td>
+                <h2>About Optic Odyssey</h2>
+
+                <p>Welcome to Optic Odyssey, a visual journey through the lens! At Optic Odyssey, we are passionate storytellers and visual artists, dedicated to capturing the beauty and essence of the world through our lenses. Our photography is more than just pictures; it's a reflection of the moments, emotions, and stories that make life extraordinary.</p>
+<br/>
+                <h2>Our Vision</h2>
+
+                <p>Our vision is to inspire and connect with people through the art of photography. We believe that every click of the shutter opens a window to a unique narrative waiting to be told. Through our lenses, we explore the world's diversity, from vibrant cityscapes to serene landscapes, from candid portraits to evocative still life.</p>
+<br/>
+                <h2>Meet the Team</h2>
+
+                <p>Our team is composed of talented and experienced photographers who share a deep love for the craft. Each of us brings a unique perspective and style to our work, ensuring a diverse range of visual stories that cater to various tastes and preferences.</p>
+            </td>
+        </tr>
+    </table>
+</div>
+<br/><br/><br/><br/><br/><br/><br/>
+<div className="footer">
+    <div className="image-buttons">
+        <a href="https://www.facebook.com"><img src="Face.png"  className="small-button"/></a>
+        <a href="https://www.instagram.com"><img src="Insta.png"  className="small-button"/></a>
+        <a href="https://twitter.com/?lang=en"><img src="X.png"  className="small-button"/></a>
     </div>
+</div>
+
+</div>
   );
 }
 
